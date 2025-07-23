559667d445d5a08019d2d3a42309b87f92451da4
Gururaj Hegde <rguru7825@gmail.com>
2025-07-23 11:39:43 +0530

    Update Register.jsx

--- Frontend/frontend/src/screens/Register.jsx
+++ Frontend/frontend/src/screens/Register.jsx
@@ -11,7 +11,7 @@ const Register = () => {
 
     function submitHandler(e){
         e.preventDefault();
-        axios.post('/users/register',{email,password}).
+        axios.post('https://devcollab-4vyp.onrender.com/users/register',{email,password}).
         then((res)=>{
             console.log(res.data)
             localStorage.setItem('token',res.data.token)
