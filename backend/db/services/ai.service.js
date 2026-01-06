09502bdacb43b535d3c9158ef28bd401983820a8
Gururaj Hegde <rguru7825@gmail.com>
2026-01-06 17:57:26 +0530

    Update AI model from 'gemini-1.0-pro' to 'gemini-pro'

--- backend/db/services/ai.service.js
+++ backend/db/services/ai.service.js
@@ -3,7 +3,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai"
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_KEY);
 const model = genAI.getGenerativeModel({
-    model: "gemini-1.0-pro",
+    model: "gemini-pro",
     generationConfig: {
         responseMimeType: "application/json",
         temperature: 0.4,
